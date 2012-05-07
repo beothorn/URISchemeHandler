8284de484e6b392a4d0e6d5066fd3895f7ad8232
Lucas <lucass@gmail.com>
2012-05-07 00:03:57 -0300

    added license

--- src/main/java/urlProtocolHandler/URLProtocolHandler.java
+++ src/main/java/urlProtocolHandler/URLProtocolHandler.java
@@ -2,12 +2,6 @@ package urlProtocolHandler;
 
 public class URLProtocolHandler {
 
-	public static void main(final String[] args) {
-		final URLProtocolHandler urlProtocolHandler = new URLProtocolHandler();
-//		urlProtocolHandler.open("magnet:?xt=urn:btih:6eee9806379ef47804658bdc35b8bb84a7f4f01e&dn=Community+S03E02+HDTV+XviD-LOL+%5Beztv%5D&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80&tr=udp%3A%2F%2Ftracker.ccc.de%3A80");
-		urlProtocolHandler.register("deleteme", "send-notify");
-	}
-	
 	private final RealURLProtocolHandler realURLProtocolHandler;
 
 	public URLProtocolHandler() {
