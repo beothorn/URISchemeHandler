26ba7820a1f09eac7500fbddfb1c968e74e5569a
David Martinez <david.martinez@strategybigdata.com>
2018-11-01 10:38:18 +0100

    Added support for mac os

--- src/main/java/uriSchemeHandler/RealURLProtocolHandlerFactory.java
+++ src/main/java/uriSchemeHandler/RealURLProtocolHandlerFactory.java
@@ -2,18 +2,18 @@ package uriSchemeHandler;
 
 public class RealURLProtocolHandlerFactory {
 
-	public static RealURISchemeHandler produce(final String osName) {
-		final String osLowerCase = osName.toLowerCase();
-		if(osLowerCase.contains("linux")){
-			return new LinuxURISchemeHandler();
-		}
-		if(osLowerCase.contains("windows")){
-			return new WindowsURISchemeHandler();
-		}
-		//For use wit osx
-		//http://ss64.com/osx/lsregister.html
-		//http://krypted.com/mac-os-x/lsregister-associating-file-types-in-mac-os-x/
-		throw new RuntimeException("OS not supported");
-	}
+    public static RealURISchemeHandler produce(final String osName) {
+        final String osLowerCase = osName.toLowerCase();
+        if (osLowerCase.contains("linux")) {
+            return new LinuxURISchemeHandler();
+        }
+        if (osLowerCase.contains("windows")) {
+            return new WindowsURISchemeHandler();
+        }
+        if (osLowerCase.contains("mac")) {
+            return new MacURISchemeHandler();
+        }
+        throw new RuntimeException("OS not supported");
+    }
 
 }
